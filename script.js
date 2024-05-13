396bde5eaa5ab1e775581d81231ea8ed63342a0d
shaan-lakhani <166409436+shaan-lakhani@users.noreply.github.com>
2024-05-13 08:36:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function runProgram(){
+async function getBookData(){
+    const baseURL = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings'
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    return data;
+}
 
+async function getBookInfo(){
+    console.log('getting book')
+
+    let bookData = await getBookData();
+    console.log("Book data:   ", bookData);
+    console.log(bookData.docs[0])
+}
+
+function runProgram(){
     console.log('runProgram');
     //your code goes here
+    let searchButton = document.querySelector("#searchinfo");
+    searchButton.addEventListener(`click`, getBookInfo);
+
+    // element.textContent = "hi"
+
+
 }
 document.addEventListener('DOMContentLoaded', runProgram);
